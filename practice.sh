2c514904ad87e44b7651373658afcf064f78485a
abhi012323 <abhisingh.gc@gmail.com>
2026-05-20 01:37:13 +0530

    variable decleration

---
+++ practice.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+name="Abhi"
+echo "My name is $name"
+echo "$name" 
+
+company="Airtel"
+echo "I'm engineer in $company"
+echo "$company"
+
+city="gurugram"
+echo "I'm based in $city"
+echo "$city"
