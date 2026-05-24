e22e7f04d4b79a6a8e35599b65edb7598530647a
abhi012323 <abhisingh.gc@gmail.com>
2026-05-24 22:16:34 +0530

    Functions

---
+++ function.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Define the function
+print_separator() {
+    echo "-----------------------------------"
+}
+
+# Use the function
+echo "Starting Backup..."
+print_separator
+echo "Backup complete!"
